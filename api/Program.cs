a688bc9c99a3ba01e5684865fc1fcac3f0957647
rupeshyy <rupeshyy@gmail.com>
2024-05-04 22:58:00 +0530

    end of section 3

--- api/Program.cs
+++ api/Program.cs
@@ -11,13 +11,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+// configure the http request pipeline
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.MapControllers();
 
 app.Run();
